3297ff4dea645975dead4f95c4307191d58c960e
Luis Rivas Lopez <luisarl355@gmail.com>
2022-11-30 22:10:42 -0500

    Refactored batch item into new component

--- Data_aggregator/src/App.jsx
+++ Data_aggregator/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import NavBar from './components/NavBar'
+import BatchItem from './components/BatchItem'
+
 
 import './App.css'
 
@@ -29,7 +31,8 @@ function App() {
         {/* <button onClick={handleClick}>
           toggle
         </button> */}
-        <h3>Batch List</h3>
+        <h3 id = "batch-title">Batch List</h3>
+        <div className="card-section">
         <div id= "batch-list">
           <ul>
             <li>Batch ID</li>
@@ -39,30 +42,11 @@ function App() {
             <li>Source</li>
             <li>Ran On</li>
           </ul>
-          <div id = "batch-items">
-            <div id = "item">
-              <div id = "batch">#32132320</div>
-              <div id = "imported">
-                <h4>250 Items</h4>
-                <div id = "import-widget">
-                  <p>100%</p>
-                  <div id = "progress-bar"></div>
-                </div>
-              </div>
-              <div id = "status">
-                <div id ="status-message">
-                  <p>Status</p>
-                </div>
-                <div id = "errors">
-                  <p>test</p>
-                </div>
-              </div>
-              <div>test</div>
-              <div>test</div>
-              <div>test</div>
-            </div>
-          </div>
+          <BatchItem></BatchItem>
         </div>
+
+        </div>
+
       </div>         
     </div>
   )
---
+++ Data_aggregator/src/components/BatchItem.jsx
@@ -0,0 +1,31 @@
+
+function BatchItem() {
+    return ( 
+        <div id = "batch-items">
+        <div id = "item">
+          <div id = "batch">#32132320</div>
+          <div id = "imported">
+            <h4>250 Items</h4>
+            <div id = "import-widget">
+              <p>100%</p>
+              <div id = "progress-bar"></div>
+            </div>
+          </div>
+          <div id = "status">
+            <div id ="status-message">
+              <p>Status</p>
+            </div>
+            <div id = "errors">
+              <p>test</p>
+            </div>
+          </div>
+          <div>test</div>
+          <div>test</div>
+          <div>test</div>
+        </div>
+      </div>
+    )
+}
+
+export default BatchItem
+  
