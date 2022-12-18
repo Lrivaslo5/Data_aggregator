750e19327e1bfb5c272d9e70e5686f3259d39c60
Luis R <76708966+Lrivaslo5@users.noreply.github.com>
2022-12-17 20:24:52 -0500

    Add interactivity to searchbar

--- Data_aggregator/src/components/Search.jsx
+++ Data_aggregator/src/components/Search.jsx
@@ -1,12 +1,41 @@
 import './Navbar.css'
+import { useState, useEffect } from 'react'
+
 
 
 function Search() {
+  const [Text, setText] = useState('')
+
+
+  function testFunc(){
+    //implment search functionality using Text state
+    setText('')
+    console.log('worked')
+  }
+  
+// this snippet allows us to submit search query using enter button
+  useEffect(() => {
+    const listener = event => {
+      if (event.code === "Enter" || event.code === "NumpadEnter") {
+        event.preventDefault();
+        testFunc(); // this funciton is called after pressing enter
+      }
+    };
+    document.addEventListener("keydown", listener);
+    return () => {
+      document.removeEventListener("keydown", listener);
+    };
+  }, []);
+  
     return (
-      <div className="bar__searchbar">
-        <img src="/src/assets/search.png"/>
-        <p>Search by ID, E-Mail, Name</p>
-      </div>
+        <input           
+          className='bar__searchbar'
+          type={'text'}
+          value={Text}
+          placeholder={'Search by ID'}
+          onChange={event => {setText(event.target.value)}}>
+        </input>
+        
     )
 }
 
